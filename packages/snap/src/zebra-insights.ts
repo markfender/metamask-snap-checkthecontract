273844118f29511cf98b116a37824f45ac8603af
Maksym F <the.mark.fender@gmail.com>
2022-11-12 19:46:10 +0100

    feature: check if contract is upgradable

--- packages/snap/src/zebra-insights.ts
+++ packages/snap/src/zebra-insights.ts
@@ -1,3 +1,5 @@
+import Web3 from 'web3';
+
 const whitelist: readonly string[] = [
   '0x326C977E6efc84E512bB9C30f76E30c160eD06FB',
 ];
@@ -28,4 +30,30 @@ export async function getZebraInsights(address: string) {
   );
   const auditResult = await auditResponse.json();
   return auditResult;
+}
+
+// eslint-disable-next-line jsdoc/require-jsdoc
+export async function checkIfUpgradable(contractAddress: string) {
+  const web3Init = new Web3(
+    new Web3.providers.HttpProvider(
+      'https://eth-mainnet.g.alchemy.com/v2/e_wJdrZ4qnlaDJ96hvsqPuySrP3qXdt6', // "https://eth-goerli.g.alchemy.com/v2/JwhZlH7Fw8FkoNVPsGmt_g9J3FnIeTT8"
+    ),
+  );
+
+  const code = await web3Init.eth.getCode(contractAddress);
+
+  const listOfUpgradableFunctions = ['implementation()', 'upgradeTo(address)'];
+  const isUpgradable = listOfUpgradableFunctions
+    .map(
+      (functionSignature: string): boolean =>
+        code.indexOf(
+          web3Init.eth.abi
+            .encodeFunctionSignature(functionSignature)
+            .slice(2, 10),
+        ) > 0,
+    )
+    .includes(true);
+
+  console.log('isSigPresent', isUpgradable);
+  return isUpgradable;
 }
